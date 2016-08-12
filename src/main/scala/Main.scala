8e74a75dcb0b0b1e983118f3abe39fdd8c95a1e1
igor <sandoler@yandex.ru>
2016-08-12 18:14:47 +0300

    Changed host

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,6 +6,6 @@ object Main extends App {
   val system = ActorSystem("system")
   val taskActor = system.actorOf(Props[TaskService], "task")
   val gmActor = system.actorOf(Props[GameService], "game")
-  val mainActor = system.actorOf(MainActor.props("192.168.211.97",8888))
+  val mainActor = system.actorOf(MainActor.props("127.0.0.1",8888))
   system.awaitTermination()
 }
